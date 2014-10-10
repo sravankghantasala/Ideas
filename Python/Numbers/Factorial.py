541f8d526eb17d91a444c878ca23a18927a01bd1
Sravan K Ghantasala <sravan.campus@gmail.com>
2014-10-10 20:07:20 +0530

    Added factorial for Python

---
+++ Python/Numbers/Factorial.py
@@ -0,0 +1,11 @@
+'''
+Factorial is already available in python
+Created on 10 Oct 2014
+
+@author: sraone
+'''
+
+from math import factorial
+
+if __name__ == '__main__':
+    print("Factorial : " , factorial(int(input('Please enter the number for which you want to find factorial for '))))
