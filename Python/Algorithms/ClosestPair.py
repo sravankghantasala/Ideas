f4a2a3f75c4f3f2a69ebf9e4804b53973d49a591
Sravan K Ghantasala <sravan.campus@gmail.com>
2014-11-12 22:28:50 +0530

    Added "Count Vowels", "Pig Latin" in strings "Closet point pair", "Collatz Conjucture" in Algorithms for python

---
+++ Python/Algorithms/ClosestPair.py
@@ -0,0 +1,41 @@
+'''
+Created on 12 Nov 2014
+
+@author: sraone
+'''
+from math import sqrt
+from collections import namedtuple
+
+Point2 = namedtuple('Point2', 'x y')
+
+class ClosestPair(object):
+    
+    def get_distance(self,p1,p2):
+        return sqrt( (p1.x - p2.x)**2 + (p1.y - p2.y)**2 )
+    
+    def get_closest_pair_for_point(self,point, points):
+        d = 0
+        for p in points:
+            if not p == point:
+                dist = self.get_distance(point, p)
+                if dist < d or d == 0: 
+                    closest=p
+                    d= dist
+        else:return (closest,d)
+                
+        
+    def get_closest_pair(self,*points):
+        closer = 0; pair = ()
+        for point in points:
+            p,d = self.get_closest_pair_for_point(point,points)
+            if d < closer or closer == 0:
+                pair=(point,p,d)
+        else: return pair
+    
+if __name__ == '__main__':
+    CP = ClosestPair()
+    p1 = Point2(9,7)
+    p2 = Point2(3,2)
+    p3 = Point2(4,3)
+    p4 = Point2(4,4)
+    print(CP.get_closest_pair(p1,p2,p3,p4))
---
+++ Python/Algorithms/Collatz_Conjucture.py
@@ -0,0 +1,39 @@
+'''Collatz Conjecture.
+
+Start with a number n > 1. 
+Find the number of steps it takes to reach one using the following process: 
+    If n is even, divide it by 2. 
+    If n is odd, multiply it by 3 and add 1.
+    
+This programs gives total steps including the final step that reached for 1
+Created on 12 Nov 2014
+
+@author: sraone
+'''
+
+class Collatz_Conjucture(object):
+    
+    def _is_even(self,num):
+        return True if num % 2 == 0 else False
+    
+    def add_magic(self,num):
+        if self._is_even(num):
+#             Divide by 2
+            return num/2
+            
+        else:
+#             Mul by 3 and add 1
+            return (num * 3) + 1
+        
+    def Conjucture(self,num):
+        steps = 1
+        while(num != 1):
+            num = self.add_magic(num)
+            steps += 1
+        else : return steps
+            
+        
+        
+if __name__ == '__main__':
+    CC = Collatz_Conjucture()
+    print(CC.Conjucture(27))
